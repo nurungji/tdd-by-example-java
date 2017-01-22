59cf15d3457103aa119716027e0dcccc835ca812
npc <npc@192.168.0.2>
2017-01-23 00:26:52 +0900

    7장. 사과와 오렌지

--- src/ex/currency/Franc.java
+++ src/ex/currency/Franc.java
@@ -9,10 +9,5 @@ public class Franc extends Money{
 	public Franc times(int multiplier) {
 		return new Franc(amount * multiplier);		
 	}	
-	
-	public boolean equals(Object object) {
-		Money money = (Money) object;
-		return amount == money.amount;
-	}
-	
+
 }
