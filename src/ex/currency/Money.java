759c5084693aef647a72b8327382016361938201
npc <npc@192.168.0.2>
2017-01-23 00:35:46 +0900

    8장. 객체 만들기

--- src/ex/currency/Money.java
+++ src/ex/currency/Money.java
@@ -1,12 +1,22 @@
 package ex.currency;
 
-public class Money {
+public abstract class Money {
+	
+	abstract Money times(int multiplier);
 	
 	protected int amount;
 	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
 		return amount == money.amount && getClass().equals(money.getClass());
+	}
+
+	public static Money dollar(int amount) {		
+		return new Dollar(amount);
+	}
+
+	public static Money franc(int amount) {
+		return new Franc(amount);
 	}
 
 }
