6c99315cc3f89ef24c81a384ca0cf16009f5848d
npc <npc@192.168.0.2>
2017-01-22 23:38:16 +0900

    1장. 다중 통화를 지원하는 Money 객

---
+++ src/ex/currency/Dollar.java
@@ -0,0 +1,14 @@
+package ex.currency;
+
+public class Dollar {
+
+	public int amount;
+	
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+
+	public void times(int multiplier) {
+		amount *= multiplier;		
+	}	
+}
