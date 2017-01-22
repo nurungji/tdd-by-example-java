e380886cb71560dfe352132857759f42ee2e7ab1
npc <npc@192.168.0.2>
2017-01-23 01:44:42 +0900

    9장. 우리가 사는 시간

--- src/ex/currency/Dollar.java
+++ src/ex/currency/Dollar.java
@@ -1,13 +1,13 @@
 package ex.currency;
 
 public class Dollar extends Money {
-
-	public Dollar(int amount) {
-		this.amount = amount;
+	
+	public Dollar(int amount, String currency) {
+		super(amount, currency);
 	}
 
 	public Money times(int multiplier) {
-		return new Dollar(amount * multiplier);
+		return Money.dollar(amount * multiplier);
 	}
 
 }
--- src/ex/currency/Money.java
+++ src/ex/currency/Money.java
@@ -5,6 +5,12 @@ public abstract class Money {
 	abstract Money times(int multiplier);
 	
 	protected int amount;
+	protected String currency;
+	
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 	
 	public boolean equals(Object object) {
 		Money money = (Money) object;
@@ -12,11 +18,15 @@ public abstract class Money {
 	}
 
 	public static Money dollar(int amount) {		
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 
 	public static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
+	}
+
+	String currency() {
+		return currency;
 	}
 
 }
